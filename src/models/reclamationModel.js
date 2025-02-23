5187dfbc780b4289ef60df558140fbc39500a79f
ayyouboubbad <ayyouboubbad12@gmail.com>
2025-02-23 13:04:27 +0000

    modification des fichier models

--- src/models/reclamationModel.js
+++ src/models/reclamationModel.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const reclamationSchema = new mongoose.Schema({
   reclamant: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  categorie: { type: String, required: true },
+  categorie: { type: String, required: true,enum:[ 
+    "Problèmes de Chambre",
+    "Repas",
+    "Hygiène",
+    "Sécurité",
+    "Infrastructure",] },
   statut: { type: String, enum: ['En cours', 'Résolu', 'Rejeté'], default: 'En cours' },
   priorite: { type: String, enum: ['Haute', 'Moyenne', 'Basse'], default: 'Moyenne' },
 }, { timestamps: true });
